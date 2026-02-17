d1ac5bf0055f86fbbb98302767b90c19aa6667ad
somemercle <89783575+somemercle@users.noreply.github.com>
2026-02-17 21:57:16 +0900

    Update vite.config.ts

--- Algostay/vite.config.ts
+++ Algostay/vite.config.ts
@@ -5,18 +5,18 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
+
+  // ✅ GitHub Pages용: 레포 이름 넣기
+  base: '/REPO_NAME/',
+
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
 
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
